180398d0c35780468b3616fbaf145c33e1e9eae5
muhammedaj2002 <muhammedaj2002@gmail.com>
2021-05-25 19:12:47 +0530

    edit

--- crml/settings.py
+++ crml/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#!@n8%(49pf-lyv&89+!^sieo=fd5o8_q!gu7eouj5g_vz2#3m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vea-mesm.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
